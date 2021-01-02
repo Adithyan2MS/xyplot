59031deacba6cca368fed7f5bad79e5e043d5d95
Adithyan2MS <68984138+Adithyan2MS@users.noreply.github.com>
2021-01-02 21:27:44 +0530

    code updated

--- xyplot.c
+++ xyplot.c
@@ -2,46 +2,92 @@
 #include "supportLib.c"
 #include <stdio.h>
 #include<stdlib.h>
+int point=0;
+struct plot
+{
+    double x;
+    double y;
+};
+int group(struct plot peak[100],int len,struct plot axis[len])
+{
+    int flag=0;
+    int k=0;
+    for(int i=0;i<len-1;i++)
+    {
+        if(axis[point].y>=0.1)
+        {
+            peak[k].x=axis[point].x;
+            peak[k].y=axis[point].y;
+            printf("%lf\t%lf\n",peak[k].x,peak[k].y);
+            point++;
+            k++;
+            flag++;
+        }
+        else if(axis[point].y<0.1)
+        {
+            if(flag>1)
+            break;
+            else
+            point++;
+        }
+    }
+}
 int main()
 {
     FILE *fp;
+    int i,j;
     fp=fopen("test.txt","r");
     if(fp==NULL)
-         printf("File not found!");
-    fseek(fp,16,SEEK_SET);
-
+    printf("File not found!");
     int len;
+    //reading datalength
+    fseek(fp,16,SEEK_SET);
     fscanf(fp,"%d",&len);
-    printf("Data length:%d\n",len);
-    double x[len],y[len];
+    printf("%d\n",len);
 
+    double a[len],b[len];
+    
+    //reading x
     fseek(fp,54,SEEK_SET);
-
-    for(int i=0;i<len;i++)
+    for(i=0;i<len;i++)
     {
-        fscanf(fp,"%lf",&x[i]);
-        printf("%lf\n",x[i]);
+        fscanf(fp,"%lf",&a[i]);
+        //printf("%lf\n",a[i]);
         fseek(fp,18,SEEK_CUR);
     }
+    
+    //reading y
     fseek(fp,70,SEEK_SET);
-
-    for(int j=0;j<len;j++)
+    for(j=0;j<len;j++)
     {
-       fscanf(fp,"%lf",&y[j]);
-       printf("%lf\n",y[j]);
-       fseek(fp,18,SEEK_CUR);
+        fscanf(fp,"%lf",&b[j]);
+        //printf("%lf\n",b[j]);
+        fseek(fp,18,SEEK_CUR); 
     }
-    free(x);
-    free(y);
+    struct plot axis[len];
+    for(int k=0;k<len;k++)
+    {
+        axis[k].x=a[k];
+        axis[k].y=b[k];
+        //printf("%lf\t%lf\n",axis[k].x,axis[k].y);
+    }
     fclose(fp);
     RGBABitmapImageReference *imageRef = CreateRGBABitmapImageReference();
 
-        DrawScatterPlot(imageRef, 1250, 800, x, 10000, y, 10000);
+        DrawScatterPlot(imageRef, 1250, 800, a, 10000, b, 10000);
 
         size_t length;
         double *pngData = ConvertToPNG(&length, imageRef->image);
         WriteToFile(pngData, length, "plot.png");
 
-        return 0;
- 
+       
+    struct plot *peak1=(struct plot*)malloc(sizeof(struct plot));
+    while(point<len-1)
+    {
+        group(peak1,len,axis);
+        printf("\n");
+    }
+    free(axis);
+    free(peak1);
+     return 0;
 }
